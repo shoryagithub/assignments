0ec19856fb654c8e1b75dddb3187def57873bb80
shorya <shorya9826@gmail.com>
2025-07-10 16:44:54 +0530

    number checking

---
+++ number_check.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+if a == 0:
+    print("The number which is given by you is zero.")
+elif a > 0:
+    print("The number which is given by you is positive.")
+else:
+    print("The number which is given by you is negative.")
